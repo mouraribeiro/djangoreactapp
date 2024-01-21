d01e1a2fa11ac958885afd881514fd496308df05
Rayanne <mouraribeiro11@gmail.com>
2024-01-21 16:33:51 -0300

    criando nested serializer, e token de autenticação

---
+++ core/serializers.py
@@ -0,0 +1,16 @@
+from .models import List, Item
+from rest_framework import serializers
+
+
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['name','done', 'url']
+
+
+class ListSerializer(serializers.HyperlinkedModelSerializer):
+    item_set = ItemSerializer(many=True)
+
+    class Meta:
+        model = List
+        fields = ['name', 'owner','item_set', 'url']
--- core/views.py
+++ core/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import permissions, viewsets, authentication
+from .models import List, Item
 
-# Create your views here.
+from .serializers import ListSerializer,ItemSerializer
+
+
+class ListViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication,authentication.SessionAuthentication]
+
